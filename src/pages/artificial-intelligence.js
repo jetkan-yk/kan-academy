e968f6586b5eb686c4bcae626e5a7bf06ec4cf70
Jet Kan <jetkan.yk@gmail.com>
2021-10-15 02:23:54 +0800

    Create AIPage

---
+++ src/pages/artificial-intelligence.js
@@ -0,0 +1,21 @@
+import * as React from "react";
+
+function AIPage() {
+  return (
+    <main>
+      <title>Jet Kan | Artificial Intelligence</title>
+      <section>
+        <h1>Want to play a game?</h1>
+        <p>Pick a game below and challenge my AI!</p>
+      </section>
+      <section>
+        <ul>
+          <li>Tic Tac Toe</li>
+          <li>Game of Nim</li>
+        </ul>
+      </section>
+    </main>
+  );
+}
+
+export default AIPage;
