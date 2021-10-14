aa012909635db924a988fb5da4f59d2a48c57d0b
Jet Kan <jetkan.yk@gmail.com>
2021-10-15 02:39:53 +0800

    Create swe page

---
+++ src/pages/swe.js
@@ -0,0 +1,21 @@
+import * as React from "react";
+
+function SWEPage() {
+  return (
+    <main>
+      <title>Jet Kan | Full Stack Development</title>
+      <section>
+        <h1>Source Academy</h1>
+        <p>
+          The Source Academy is an online learning platform for CS1101S
+          Programming Methodology class in NUS. Every year, this website is
+          utilized by 500+ NUS students to write programs, submit homework and
+          review learning progress.
+        </p>
+        <p>Explain my internship project here.</p>
+      </section>
+    </main>
+  );
+}
+
+export default SWEPage;
