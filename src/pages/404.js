baec02cac3aece9b6a5e5782b7eaf27cbada817d
Jet Kan <jetkan.yk@gmail.com>
2021-10-15 16:42:18 +0800

    Add jerry

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,15 +1,16 @@
-import * as React from "react";
 import { Link } from "gatsby";
+import { StaticImage } from "gatsby-plugin-image";
+import * as React from "react";
 
 function NotFoundPage() {
   return (
     <main>
       <title>Jet Kan | Not found</title>
       <h1>Page not found</h1>
-      <p>
-        Oops! Something went wrong.
-        <Link to="/">Go home</Link>.
-      </p>
+      <p>Oops! Something went wrong.</p>
+      <Link to="/">Go home</Link>
+      <br></br>
+      <StaticImage src="../images/jerry-question-mark.jpg" alt="404" />
     </main>
   );
 }
