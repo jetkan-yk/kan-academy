eb355cb2c4655d4ac3427e5d5a4f3a31d1e57714
Jet Kan <jetkan.yk@gmail.com>
2021-10-19 22:37:43 +0800

    Populate Index Page Fixes #28

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,9 +6,12 @@ function IndexPage() {
   return (
     <Page pageTitle="AI Engineer & Full Stack Developer">
       <section>
-        <h1>Welcome to Kan Academy!</h1>
+        <h1>Hi, my name is Jet Kan</h1>
+        <p>-Tag line here-</p>
+      </section>
+      <section>
         <p>
-          I'm a NUS Computer Science undergraduate specialized in Artificial
+          I'm a NUS Computer Science undergraduate specializing in Artificial
           Intelligence. I love learning new things and sharing with people.
         </p>
       </section>
@@ -22,6 +25,14 @@ function IndexPage() {
             <Link to="/software-engineering">Software Engineering</Link>
           </li>
         </ul>
+      </section>
+      <section>
+        <h1>Get In Touch</h1>
+        <p>
+          I'm graduating by December 2021 and am actively looking for
+          opportunities in the AI field. Feel free to say hi!
+        </p>
+        <a href="mailto: jetkan.yk@gmail.com">Say Hi</a>
       </section>
     </Page>
   );
